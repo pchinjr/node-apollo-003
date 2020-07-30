3bfedf1bf6858eaeddc296f79112113a5933b2c6
Paul Chin Jr <pchinjr@gmail.com>
2020-07-29 20:52:33 -0400

    rename lib

--- src/http/post-graphql/env.js
+++ src/http/post-graphql/env.js
@@ -1,2 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
+"use strict";
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv.default.config({
+  path: '.env'
+});

9f311fc97e56b82d1d6d043b2f1b0e79d231ffb0
Paul Chin Jr <pchinjr@gmail.com>
2020-07-30 12:21:25 -0400

    removed src

--- src/http/post-graphql/env.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _dotenv = _interopRequireDefault(require("dotenv"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_dotenv.default.config({
-  path: '.env'
-});
